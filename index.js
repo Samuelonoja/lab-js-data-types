a2c9d0dfafc530066f4a926d56406d394bd17e3a
Samuel Onoja <samuelonoja0@gmail.com>
2025-01-15 13:33:22 -0800

    lab done

--- index.js
+++ index.js
@@ -8,26 +8,23 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+console.log(s1 + s2 + s3 + s4 + s5 + s3 + s2 + s1 + s5);
 
 // Print out the concatenated string
-
-
-
 
 /*******************************************
     Iteration 1.2 | Camel Tail
 *******************************************/
 const part1 = "java";
 const part2 = "script";
+let result;
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
-
+result = part1[-1].toUpperCase() + part2[-1].toUpperCase();
 // Print the cameLtaiL-formatted string
 
-
-
+console.log(result);
 
 /*******************************************
     Iteration 2.1 | Calculate Tip
@@ -35,12 +32,11 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+let tipAmount = (15 / 100) * billTotal;
 
 // Print out the tipAmount
 
-
-
+console.log(tipAmount);
 
 /*******************************************
     Iteration 2.2 | Generate Random Number
@@ -48,10 +44,14 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
+function printRandom(min, max) {
+  return Math.random() * (max - min) + min;
+}
+let randomNumber = printRandom(1, 10);
 
 // Print the generated random number
 
-
+console.log(parseInt(randomNumber));
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -62,15 +62,16 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
-
+console.log(expression1);
 const expression2 = a || b;
-
+console.log(expression2);
 const expression3 = !a && b;
-
+console.log(expression3);
 const expression4 = !(a && b);
-
+console.log(expression4);
 const expression5 = !a || !b;
-
+console.log(expression5);
 const expression6 = !(a || b);
-
+console.log(expression6);
 const expression7 = a && a;
+console.log(expression7);
